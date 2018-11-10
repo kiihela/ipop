10f3cf542598c3c92e085289f8960868acada47e
Pascal Dennerly <dnnrly@gmail.com>
2018-11-10 22:45:02 +0000

    Increase coverage

--- connection_test.go
+++ connection_test.go
@@ -4,10 +4,9 @@ import (
 	"log"
 	"testing"
 
+	"github.com/dnnrly/ipop/testdata/models"
 	"github.com/gobuffalo/pop"
 	"github.com/stretchr/testify/assert"
-
-	"github.com/dnnrly/ipop/testdata/models"
 )
 
 var (
@@ -25,31 +24,74 @@ func init() {
 		log.Panic(err)
 	}
 
-	connection.MigrateReset("testdata/migrations")
-	
+	migrator, err := pop.NewFileMigrator("testdata/migrations", connection)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = migrator.Reset()
+	if err != nil {
+		log.Panic(err)
+	}
+
 	db = NewConnectionAdapter(connection)
 }
 
-func TestConnectionAdapter_SaveAndFind(t *testing.T) {
+func TestConnectionAdapter_SaveAndFindAndUpdate(t *testing.T) {
 	user := models.User{
 		Name: "Bob",
 	}
 
 	err := db.Save(&user)
 	assert.NoError(t, err)
+	origUpdated := user.UpdatedAt
 
 	found := models.User{}
 	err = db.Find(&found, user.ID)
 	assert.NoError(t, err)
 	assert.False(t, found.CreatedAt.IsZero())
+
+	updated := models.User{
+		ID:   user.ID,
+		Name: "Another name",
+	}
+
+	err = db.Update(&updated)
+	assert.NoError(t, err)
+	assert.False(t, found.UpdatedAt.IsZero())
+	assert.NotEqual(t, origUpdated.String(), found.UpdatedAt.String())
+}
+
+func TestConnectionAdapter_CreateAndFirst(t *testing.T) {
+	user := models.User{
+		Name: "Alice",
+	}
+
+	err := db.Create(&user)
+	assert.NoError(t, err)
+
+	found := models.User{}
+	err = db.First(&found)
+	assert.NoError(t, err)
+}
+
+func TestConnectionAdapter_ErrorCallback(t *testing.T) {
+	called := false
+
+	err := db.Transaction(func(tx Connection) error {
+		called = true
+		return nil
+	})
+
+	assert.NoError(t, err)
+	assert.True(t, called)
 }
 
 func TestConnectionAdapter_Callback(t *testing.T) {
 	called := false
 
-	err := db.Transaction(func (tx Connection) error {
+	err := db.Rollback(func(tx Connection) {
 		called = true
-		return nil
 	})
 
 	assert.NoError(t, err)
